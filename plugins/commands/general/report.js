76c2ddd9060cfd8b7428c1e347914d6a02cb1908
Vasilyok5 <131940628+Vasilyok5@users.noreply.github.com>
2023-05-05 13:31:10 +0700

    Update report.js

--- plugins/commands/general/report.js
+++ plugins/commands/general/report.js
@@ -9,13 +9,13 @@ const config = {
 
 const langData = {
     "vi_VN": {
-        "report_no_content": "❎ Bạn chưa nhập/reply tin nhắn nào để report hoặc file đính kèm không được hỗ trợ!",
+        "report_no_content": "❌ Bạn chưa nhập/reply tin nhắn nào để report hoặc file đính kèm không được hỗ trợ!",
         "report_content": "📢 Báo cáo từ {reporterName} ({senderID})\n📌 Thread: {reporterThreadName} ({threadID})\n📝 Nội dung:\n{content}",
-        "report_failed": "❎ Gửi báo cáo thất bại, vui lòng thử lại sau!",
+        "report_failed": "❌ Gửi báo cáo thất bại, vui lòng thử lại sau!",
         "report_success": "✅ Đã gửi báo cáo đến {reportSuccess} quản trị bot!",
-        "not_mod": "❎ Bạn không phải là quản trị viên của bot!",
+        "not_mod": "❌ Bạn không phải là quản trị viên của bot!",
         "reply_content": "📬 Phản hồi từ {senderName} ({senderID})\n📝 Nội dung:\n{content}",
-        "error": "❎ Đã có lỗi xảy ra, vui lòng thử lại sau!"
+        "error": "❌ Đã có lỗi xảy ra, vui lòng thử lại sau!"
     }
 }
 
